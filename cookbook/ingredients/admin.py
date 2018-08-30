7fb81cd0656c4ff936479b3174d7bc67b7037ac4
Lucas Ferrario <lucas.ferrario@bigbox.com.ar>
2018-08-30 17:13:24 -0300

    first commit

---
+++ cookbook/ingredients/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from django.contrib import admin
+from cookbook.ingredients.models import Category, Ingredient
+
+admin.site.register(Category)
+admin.site.register(Ingredient)

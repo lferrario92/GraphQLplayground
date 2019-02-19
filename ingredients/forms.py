483aa8327bf20f853b5930c37a3264503d482b0c
lferrario <lferrario92@gmail.com>
2019-02-19 17:58:44 -0300

    todo implement category creation form

--- ingredients/forms.py
+++ ingredients/forms.py
@@ -7,3 +7,8 @@ class IngredientForm(forms.ModelForm):
     class Meta:
         model = Ingredient
         fields = ('name', 'notes', 'category')
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ('name')
